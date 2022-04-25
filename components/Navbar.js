335c01a158252697aab0eb7abc079207731b353f
Abdul <abdul.rehmtulla@paypal.com>
2022-04-25 15:01:39 +0300

    init

---
+++ components/Navbar.js
@@ -0,0 +1,16 @@
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
+
+export default function Navbar() {
+  return (
+    <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+        <Toolbar>
+          <Typography variant="h6" noWrap component="div">
+            Home
+          </Typography>
+        </Toolbar>
+      </AppBar>
+  );
+}
